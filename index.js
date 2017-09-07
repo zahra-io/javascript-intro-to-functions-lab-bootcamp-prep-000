249ea830173b1041e1dc8ddee66e2933635c757e
Zahra Khan <zarssskhan@gmail.com>
2017-09-07 05:01:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,7 @@ function sayHiToGrandma(string) {
     return 'I can\'t hear you!';
   } else if (string.toUpperCase()) {
     return "YES INDEED!"
-  } else if (string === "I love you, Grandma.")
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 };
